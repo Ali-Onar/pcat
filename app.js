0d19b0445d79aa325ba311240ddf20be3d36678c
ali tunacan onar <alitunacanonar@icloud.com>
2022-10-21 12:31:30 +0300

    Express and Nodemon installed

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: "Pencil",
+        description: "Pencil blah blah"
+    }
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('Sunucu $(port) portunda başlatıldı.');
+});
